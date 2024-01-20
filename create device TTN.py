216b7f1b1363f549c213c8a1713f133e501a22fc
FK-sauve <126117269+FK-sauve@users.noreply.github.com>
2024-01-20 11:00:25 +0100

    Update create device TTN.py

--- create device TTN.py
+++ create device TTN.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jan 10 04:05:02 2024
-
+This script demonstrate TTN device approvisionning
+Python 3.11 Windows and linux work's well
 @author: FSauve
 """
 
 import requests
 import json
 
-
+# Credentials and all that we need to create
 app_id = 'test-all'
 access_key = '.......................4F4BUR4ANY.GFXCOLU22BOUARVUOOMPAE22INOOHTQBUWSTP2R3FFVPEI6GQJ2A'
 dev_id = 'eui-70b3d57ed0063abd'
@@ -18,7 +19,8 @@ app_key = 'E8C54D73F451C89A1C402C49DF27DABD'
 # URL de l'API TTN V3 pour approvisionner un dispositif en ABP
 api_url = f'https://eu1.cloud.thethings.network/api/v3/as/applications/{app_id}/devices/{dev_id}'
 
-# Données du dispositif LoRaWAN
+# Donnees du dispositif LoRaWAN
+# Data to create device value to TTN
 device_data = {
     "ids": {"device_id": dev_id},
     "lorawan_device": {
@@ -40,15 +42,18 @@ headers = {
 }
 
 # Envoi de la requête HTTP PUT pour approvisionner le dispositif
+# Send HTTP request to TTN with credentials 
 response = requests.put(api_url, headers=headers, data=json.dumps(device_data))
 
 # Vérification du code de statut de la réponse
 if response.status_code == 200:
+    # response 200 = OK this device is create
     print(f"Dispositif {dev_id} approvisionné avec succès en mode ABP avec reset des compteurs.")
     print(response.text)
     print('--------------')
     print(' status code:', response.status_code)
 else:
+    # HTTP response != 200 so no creation
     print(f"Erreur lors de l'approvisionnement du dispositif. Code de statut : {response.status_code}")
     print(response.text)
 
